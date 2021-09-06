0c42d499af74edd56f957ad9e6265901179f0ba9
singh.kulwant <singh.kulwant@gmx.com>
2021-09-07 00:03:26 +0200

    09-06: adapter pattern added

---
+++ src/main/java/com/design/main/DesignPatternApplication.java
@@ -0,0 +1,11 @@
+package com.design.main;
+
+import com.design.adapterpattern.AdapterPatternMain;
+
+public class DesignPatternApplication {
+
+	public static void main(String[] args) {
+		AdapterPatternMain.run();
+	}
+
+}
