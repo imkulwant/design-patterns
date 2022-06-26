62ee75348685e8f881cab0a623170ed8b32ee264
singh-kulwant <singh.kulwant@gmx.com>
2022-06-26 23:57:05 +0200

    06-26: removed unused dependencies

--- src/test/java/com/designpattern/DesignPatternApplicationTests.java
+++ src/test/java/com/designpattern/DesignPatternApplicationTests.java
@@ -1,8 +1,5 @@
 package com.designpattern;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 class DesignPatternApplicationTests {
 
 	void contextLoads() {
